2c59fe0bc32de56ef7b4b1ff2979505c72461db5
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-06-15 23:43:15 +0100

    first-commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 'use strict';
-/////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
@@ -8,12 +7,13 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
 
+function sum(a, b) {
+  let c = a + b 
+  let d = [c, "The sum of "+ a +" and "+ b +" is " + c + "."]
+  return d
 }
-
-// Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -26,12 +26,13 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(a, b) { 
+    let c = a * b
+    let d = [c , "The product of "+a+" and " +b+" is " + c + "."]
+    return d
 }
+testMultiply(5,9);
 
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
