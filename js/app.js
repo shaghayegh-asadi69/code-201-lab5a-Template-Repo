2b230341850dcaf612adaa5ffd3b9b7440e521e5
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-06-16 04:21:44 +0100

    update

--- js/app.js
+++ js/app.js
@@ -10,8 +10,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 function sum(a, b) {
   let c = a + b 
-  let d = [c, "The sum of "+ a +" and "+ b +" is " + c + "."]
-  return d
+  let d = "The sum of "+ a +" and "+ b +" is " + c + "."
+  
+  return [c,d]
 }
 testSum(4, 7);
 
@@ -27,9 +28,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { 
-    let c = a * b
-    let d = [c , "The product of "+a+" and " +b+" is " + c + "."]
-    return d
+    let c = a * b ;
+    let d = "The product of "+ a +" and " + b +" is " + c + "."
+    return [c, d]
 }
 testMultiply(5,9);
 
@@ -47,13 +48,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+//console.log(sum(1,2)[0]);
+//console.log(multiply(1,2)[0]);
 
+function sumAndMultiply(a, b, c){
+     let sum1 = sum(sum(a, b)[0], c)[0]
+     let product1 = multiply(multiply(a, b)[0], c)[0]
+     let mssg1 = a + " and " +  b + " and "+ c + " sum to " + sum1 + "."
+     let mssg2 = "The product of " + a + " and " + b + " and " + c + " is " + product1 + ".";
+    
+
+   return [sum1, product1, mssg1, mssg2];
 }
-
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
+//console.log(sumAndMultiply(1,2));
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -70,13 +78,17 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
+function sumArray(sumArr) { 
+    let suml = sum(testArray[0], testArray[1])[0];
+    let finalSum = sum(suml, testArray[2])[0];
+    let array = [finalSum, testArray[0] +"," + testArray[1] + ","+ testArray[2] + "was passed in as an array of numbers, and " + finalSum +"is their sum."];
+    return array;
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+//testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
